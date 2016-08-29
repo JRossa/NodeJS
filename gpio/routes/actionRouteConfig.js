aeba1bb4c1703c4b2cf2a524905811f2edf5b2e9
José Rossa <jose.rossa@waymotion.com>
2016-08-29 19:29:42 +0100

    2016-08-29 - sqlitePinDao sequential

--- gpio/routes/actionRouteConfig.js
+++ gpio/routes/actionRouteConfig.js
@@ -110,26 +110,27 @@ actionRouteConfig.prototype.dbCreateTable = function () {
     actionDao = require('../system/dao/mysqlActionDao');
   }
 
-  actionDao.actionDao.createTableActionType( function (status) {
+    actionDao.actionDao.createTableActionType( function (status) {
 
-    if (status.status == "Finished") {
-      console.log("Insert Action Types !!")
-      self.dbCreateActionType();
+      if (status.status == "Finished") {
+        console.log("Insert Action Types !!")
+        self.dbCreateActionType();
 
-      actionDao.actionDao.createTableAlarmPeriod(function (status) {
+        actionDao.actionDao.createTableAlarmPeriod(function (status) {
 
-        if (status.status == "Finished") {
-          console.log("Insert Initial Period !!")
-          self.dbCreateAlarmPeriod();
+          if (status.status == "Finished") {
+            console.log("Insert Initial Period !!")
+            self.dbCreateAlarmPeriod();
 
-          actionDao.actionDao.createTableAction();
-        }
-      });
-    }
+            actionDao.actionDao.createTableAction();
+          }
+        });
+      }
 
-  });
+    });
 
-/*  // sequential
+  } else {
+
     actionDao.actionDao.createTableAction();
 
     actionDao.actionDao.createTableActionType( function (status) {
@@ -147,7 +148,7 @@ actionRouteConfig.prototype.dbCreateTable = function () {
         self.dbCreateAlarmPeriod();
       }
     });
-*/
+  }
 
 /* for testing data storage
   actionDao.actionDao.getAlarmPeriod (1,
